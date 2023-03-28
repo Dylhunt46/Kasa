35db70bea9d3af14edcc3ce1c2c6a1513239b6a4
Laurent Houssin <dylhunt46@gmail.com>
2023-03-28 16:35:31 +0200

    Start project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
