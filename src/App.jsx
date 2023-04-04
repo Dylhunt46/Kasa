33880dc85fbf881406862342d58f7a3cce051a3f
Laurent Houssin <dylhunt46@gmail.com>
2023-04-04 17:57:59 +0200

    Create layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Main from './components/Main.jsx';
 import Banner from './components/Banner';
 import Gallery from './components/Gallery';
-import Footer from './components/Footer.jsx';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Main>
-        <Banner />
-        <Gallery />
-      </Main>
-      <Footer />
-    </div>
+    <>
+      <Banner />
+      <Gallery />
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navbar from './layout/Navbar';
+import Main from './layout/Main';
+import Footer from './layout/Footer';
+
+const HeaderFooterLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Main>
+        <Outlet />
+      </Main>
+      <Footer />
+    </>
+  );
+};
 
 const routeur = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <HeaderFooterLayout />,
     errorElement: <h1>404 Not found</h1>,
-  },
-  {
-    path: '/accomodation',
-    element: <h1>Nos logements</h1>,
-  },
-  {
-    path: '/about',
-    element: <h1>A propos</h1>,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/accomodation',
+        element: <h1>Logements</h1>,
+      },
+      {
+        path: '/about',
+        element: <h1>A proposs</h1>,
+      },
+    ],
   },
 ]);
 
