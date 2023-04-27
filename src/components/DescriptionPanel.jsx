4254f49c3006e047b516f98fcfb78d5d8b188fdd
Laurent Houssin <dylhunt46@gmail.com>
2023-04-27 17:24:54 +0200

    Modif collaps

--- src/components/DescriptionPanel.jsx
+++ src/components/DescriptionPanel.jsx
@@ -8,12 +8,14 @@ function DescriptionPanel(props) {
   };
   const contentClass =
     (isContentVisible ? 'visible' : 'hidden') + 'description__content';
+  const chevronClass =
+    (isContentVisible ? 'fa-chevron-up' : 'fa-chevron-down') + ' fas';
   return (
     <div className="description__panel">
       <p className="description__header">
         <span>{props.title}</span>
         <i
-          className="fa-sharp fa-solid fa-chevron-up"
+          className={chevronClass} //"fa-sharp fa-solid fa-chevron-up"
           onClick={showContent}
         ></i>
       </p>
