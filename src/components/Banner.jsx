0cdde4e072f49866866662a7e43fdfabf27faad4
Laurent Houssin <dylhunt46@gmail.com>
2023-05-04 17:51:48 +0200

    Work on responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,9 @@ import './banner.scss';
 function Banner() {
   return (
     <div className="banner">
-      <h2>Chez vous, partout et ailleurs</h2>
+      <h2>
+        Chez vous, <span className="text__intro">partout et ailleurs</span>
+      </h2>
     </div>
   );
 }
